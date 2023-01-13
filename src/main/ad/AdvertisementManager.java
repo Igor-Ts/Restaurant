0b378417760661c0e4e3cb1a4f93abc9cc72cd81
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-01-13 21:35:20 +0200

    Finished task 7. Added Advertisement videos and a few methods how to work with videos. one of it is singleton class. Added task 8.

--- src/main/ad/AdvertisementManager.java
+++ src/main/ad/AdvertisementManager.java
@@ -1,4 +1,6 @@
 package main.ad;
 
 public class AdvertisementManager {
+
+    private final AdvertisementStorage storage = new AdvertisementStorage();
 }
--- src/main/ad/AdvertisementStorage.java
+++ src/main/ad/AdvertisementStorage.java
@@ -1,13 +1,32 @@
 package main.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage {
+    public AdvertisementStorage() {
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min
+    }
+
     private static AdvertisementStorage instance;
+    final List <Advertisement> videos = new ArrayList<>();
 
     public static AdvertisementStorage getInstance() {
         if (instance == null) {
             instance = new AdvertisementStorage();
         }
         return instance;
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 
 }
