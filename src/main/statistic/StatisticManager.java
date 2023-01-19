2364b58adf8124a9c54816a3ab39df02276d77ab
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-01-19 19:57:37 +0200

    Finished task 13.Added inner class Statistic storage. Manager connected to storage 1 by 1. Added task 14.

--- src/main/statistic/StatisticManager.java
+++ src/main/statistic/StatisticManager.java
@@ -1,10 +1,24 @@
 package main.statistic;
 
+import main.kitchen.Dish;
 import main.statistic.event.EventDataRow;
+import main.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
-
     private static StatisticManager managerInstance = new StatisticManager();
+    private StatisticStorage statisticStorage = new StatisticStorage();
+    public StatisticManager() {
+        for (EventType eventType: EventType.values()) {
+            statisticStorage.storage.put(eventType, new ArrayList<>());
+        }
+    }
+
+
 
     public static StatisticManager getInstance() {
         return managerInstance;
@@ -13,4 +27,10 @@ public class StatisticManager {
     public void register(EventDataRow data){
 
     }
+
+    private class StatisticStorage {
+        Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+    }
+
 }
